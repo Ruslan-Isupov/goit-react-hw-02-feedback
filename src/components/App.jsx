d12aca0733bc9387e1e34be892731867763e0e73
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-03-23 02:13:02 +0200

    Add styled components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+
+
+export class MyClassComponent extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleIncrement = e => {
+    this.setState(prevState => {
+      if (e.target.id === 'good') {
+        return { good: prevState.good + 1 };
+      } else if (e.target.id === 'neutral') {
+        return { neutral: prevState.neutral + 1 };
+      } else if (e.target.id === 'bad') {
+        return { bad: prevState.bad + 1 };
+      }
+    });
+  };
+  countTotalFeedback = () => {
+    
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    
+    return Math.round(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100
+    );
+  };
+  render() {
+    const {good,neutral,bad} = this.state
+
+    const totalFeedback = this.countTotalFeedback();
+    
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    
+    return (
+      <Section title="Please Leave Feedback">
+        <FeedbackOptions
+          
+
+          onLeaveFeedback={this.handleIncrement}
+        />
+
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      </Section>
+    );
+  }
+}
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,7 @@
+import { MainContainer, Heading } from "./Section.styled"
+
+export const Section = ({ title, children }) => {
+    return <MainContainer><Heading>{title}</Heading>
+    {children}
+    </MainContainer>
+}
